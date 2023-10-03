f55a8bdff4dd2611e50efd5f4470613b0c742917
oghenrique <santosguhenrique@gmail.com>
2023-10-03 08:52:01 -0300

    corrigindo layout

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 'use strict'
-const scrollText = document.querySelector(".scroll-text");
-
-scrollText.addEventListener("animationiteration", () => {
-  scrollText.style.animation = "none"; // Remove a animação
-  void scrollText.offsetWidth; // Trigger a reflow para reiniciar a animação imediatamente
-  scrollText.style.animation = "scroll 30s linear infinite"; // Reaplica a animação
-});
