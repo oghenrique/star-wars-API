a770c928f7f5db1749514e233b649e945c492d94
oghenrique <santosguhenrique@gmail.com>
2023-10-08 21:57:50 -0300

    falta estilização

--- app.js
+++ app.js
@@ -1,41 +1,12 @@
 'use strict'
 
-function alternarAudio(audio, iconeAudio, musica) {
-    if (audio.paused) {
-        audio.play()
-        iconeAudio.classList.add('audio-playing')
-        musica.classList.add('audio-playing')
-    } else {
-        audio.pause()
-        iconeAudio.classList.remove('audio-playing')
-        musica.classList.remove('audio-playing')
-    }
-}
-
-function reproduzir(iconeAudio, musica) {
-    iconeAudio.classList.add('audio-playing')
-    musica.classList.add('audio-playing')
-}
-
-function pausa(iconeAudio, musica) {
-    iconeAudio.classList.remove('audio-playing')
-    musica.classList.remove('audio-playing')
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    const audio = document.getElementById('audioPlayer')
-    const iconeAudio = document.getElementById('audioIcon')
-    const musica = document.getElementById('customAudio')
-
-    musica.addEventListener('click', function () {
-        alternarAudio(audio, iconeAudio, musica)
+    
+    const audio = document.getElementById('audio-player')
+    audio.addEventListener('ended', function () {
+        audio.currentTime = 0
+        audio.play()
     })
 
-    audio.addEventListener('play', function () {
-        reproduzir(iconeAudio, musica)
-    })
-
-    audio.addEventListener('pause', function () {
-        pausa(iconeAudio, musica)
-    })
+    audio.play()
 })
